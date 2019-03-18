1c3e2a112d372cd3c98661da5f5a58a78c9528d0
Timothy Sherry <timsherry97@gmail.com>
2019-03-18 03:45:03 -0400

    Graphed and Fit to line

--- fit.py
+++ fit.py
@@ -11,10 +11,12 @@ def main():
             # This ternary operator below just assigns to each x value a y value
             y.append(float(line)) if len(x) > len(y) else x.append(float(line))
 
+
     d = len(x) - 1  # The degree of the polynomial we will use to fit the points
     p = len(x)  # The number of points we have
     w = np.random.rand(1, d+1)    # Generate random weights for the vector
 
+    # Generate a
     a = np.empty([p, p])
     for i in range(0,p):
         for j in range(0,p):
@@ -23,12 +25,35 @@ def main():
     # Use these as matrices
     a = np.asmatrix(a)
     w = np.asmatrix(w)
-    y1 = np.asmatrix(y)
+    yMatrix = np.asmatrix(y)
     y2 = a*w.transpose()
 
-#    plt.plot(x,f1,color="red") # Plot the eventual function we will implement
-#    plt.plot(x, y, 'ro')    # Plot the points
-#    plt.show()  # Displaying the figures
+    # Calculate the minimized weight vector
+    w = np.linalg.inv(a.transpose()*a)
+    w = w*a.transpose()
+    w = w*yMatrix.transpose()
+
+
+    # Create the function
+    x24 = np.arange(0,10)
+    wNew = np.squeeze(np.asarray(w))
+    f1 = (x24**0)*wNew[0]
+    for value in range(1, d+1):
+        f1 += (x24**value)*wNew[value]
+
+    # Calculate the average square error over all the points
+    ew = (a*w) - yMatrix
+
+    # Print data to console:
+    print("Weight Vector: \n" + str(w))
+    print("y values: \n" + str(yMatrix))
+    print("y tilde values: \n" + str(a*w))
+    print("Average squared error: " + str((np.linalg.norm(ew)**2)*(1/p)))
+
+    # Plot the function and the points, then show
+    plt.plot(x24, f1, color="red")
+    plt.plot(x, y, 'ro')
+    plt.show()
 
 if __name__ == "__main__":
     main()
